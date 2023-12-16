c6c9313be533c7878ae687a7995d847097351013
jlima004 <jlima004@gmail.com>
2023-12-16 17:34:29 -0300

    add: authentication & tests E2E

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
+
 import { AppModule } from './app.module'
+import { ENV } from './env'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3333)
+  const app = await NestFactory.create(AppModule, {
+    // logger: false,
+  })
+
+  const configService: ConfigService<ENV, true> = app.get(ConfigService)
+  const port = configService.get('PORT', { infer: true })
+
+  await app.listen(port)
 }
 
 bootstrap()
